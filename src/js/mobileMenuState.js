6053da430f274032b6d8cbb3a13fba823278e3cb
newdayathome <zmed2013@yandex.ru>
2019-03-06 15:55:26 +0300

    added class .opened for header state transform

--- src/js/mobileMenuState.js
+++ src/js/mobileMenuState.js
@@ -1,53 +1,8 @@
-// const mobileMenuButton = document.querySelector('.mobile-menu');
-// const closeMenuItem = document.querySelector('.close-menu_mobile');
-
-// function mobileMenuState() {
-//   const menuItem = document.querySelectorAll('.menu-item');
-
-//   const upperLine = document.querySelector('.line_upper');
-//   const middleLine = document.querySelector('.line_middle');
-//   const lowerLine = document.querySelector('.line_lower');
-
-//   if (
-//     !upperLine.classList.contains('line_upper_transform') &&
-//     !middleLine.classList.contains('line_middle_transform') &&
-//     !lowerLine.classList.contains('line_lower_transform')
-//   ) {
-//     upperLine.classList.add('line_upper_transform');
-//     middleLine.classList.add('line_middle_transform');
-//     lowerLine.classList.add('line_lower_transform');
-
-//     menuItem.forEach(item => {
-//       if (item === menuItem[0]) {
-//         return;
-//       } else {
-//         item.classList.add('menu_opened');
-//       }
-//     });
-//     closeMenuItem.classList.add('close-menu_mobile_opened');
-//   } else {
-//     upperLine.classList.remove('line_upper_transform');
-//     middleLine.classList.remove('line_middle_transform');
-//     lowerLine.classList.remove('line_lower_transform');
-
-//     menuItem.forEach(item => {
-//       if (item === menuItem[0]) {
-//         return;
-//       } else {
-//         item.classList.remove('menu_opened');
-//       }
-//     });
-//     closeMenuItem.classList.remove('close-menu_mobile_opened');
-//   }
-// }
-
-// mobileMenuButton.addEventListener('touchend', mobileMenuState);
-// closeMenuItem.addEventListener('touchend', mobileMenuState);
-
 const menu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-menu_mobile');
 
 function addingMenuState() {
+  const headerBlock = document.querySelector('.header');
   const firstLine = document.querySelector('.line_upper');
   const thirdLine = document.querySelector('.line_lower');
   const secondLine = document.querySelector('.line_middle');
@@ -63,6 +18,7 @@ function addingMenuState() {
     if (item === menuItems[0]) {
       return;
     } else {
+      headerBlock.classList.toggle('opened');
       item.classList.toggle('menu_opened');
       closeMenu.classList.toggle('close-menu_mobile_opened');
     }
