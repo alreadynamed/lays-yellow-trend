2881ed8872b38b4df11a1715525be4cbdbf9cca9
meevg <zmed2013@yandex.ru>
2019-03-13 20:55:06 +0300

    added hiding the menu when swipe

--- src/js/headerSwipeState.js
+++ src/js/headerSwipeState.js
@@ -11,12 +11,35 @@ function getFirstTouch(event) {
 
 function headerBlockMoveState(event) {
   const headerBlock = document.querySelector('.header');
+  const menuItems = document.querySelectorAll('.menu-item');
+  const closeMenu = document.querySelector('.close-menu_mobile');
+
+  const menu = document.querySelector('.mobile-menu');
+  const firstLine = document.querySelector('.line_upper');
+  const thirdLine = document.querySelector('.line_lower');
+  const secondLine = document.querySelector('.line_middle');
+
   let swipeUp = event.touches[0].clientY;
   let swipeDiff = swipeDown - swipeUp;
 
   if (swipeDiff > 0) {
     if (headerBlock.classList.contains('opened')) {
-      headerBlock.style.transform = '';
+      menuItems.forEach(item => {
+        if (item === menuItems[0]) {
+          return;
+        } else {
+          headerBlock.classList.remove('opened');
+          item.classList.remove('menu_opened');
+          closeMenu.classList.remove('close-menu_mobile_opened');
+
+          menu.classList.remove('mobile-menu_opened');
+          firstLine.classList.remove('line_upper_transform');
+          secondLine.classList.remove('line_middle_transform');
+          thirdLine.classList.remove('line_lower_transform');
+        }
+      });
+
+      // headerBlock.style.transform = '';
     } else {
       headerBlock.classList.add('header_hide');
     }
